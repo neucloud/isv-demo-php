9adb703c1eab9ac1d93a44eae769332f72e12a01
zhangweiwei <zhangweiwei@neucloud.cn>
2016-10-17 22:38:51 +0800

    接口示例代码

---
+++ Api.php
@@ -0,0 +1,111 @@
+<?php
+class Api
+{
+    const CLIENT_ID = 'CLIENT_ID';
+    const CLIENT_SECRET = 'CLIENT_SECRET';
+
+    /**
+    *开通示例
+    **/
+    public function open()
+    {
+        //处理具体业务逻辑
+        //请不要阻塞此接口，若耗时较长，可使用队列做缓冲，设置status="executing"，然后立即返回。
+        //..
+
+        //返回结果
+        $result = array();
+        $result['app_id'] = 'abcde12';
+        $result['status'] = 'opened';
+        $result['outputs'] = ['url' => 'http://isv.com/sso.php'];
+        return $this->generateCorrectResponse($result);
+    }
+
+    /**
+    *关闭示例
+    **/
+    public function close()
+    {
+        //处理具体业务逻辑
+        //..
+
+        //返回结果
+        $result = array();
+        $result['status'] = 'closed';
+        return $this->generateCorrectResponse($result);
+    }
+
+    /**
+    *删除示例
+    **/
+    public function delete()
+    {
+        //处理具体业务逻辑
+        //..
+
+        //返回结果
+        $result = array();
+        $result['status'] = 'deleted';
+        return $this->generateCorrectResponse($result);
+    }
+
+    /**
+    *续费示例
+    **/
+    public function renew()
+    {
+        //处理具体业务逻辑
+        //..
+
+        //返回结果
+        $result = array();
+        $result['status'] = 'opened';
+        return $this->generateCorrectResponse($result);
+    }
+
+    /**
+    *升级示例
+    **/
+    public function change()
+    {
+        //处理具体业务逻辑
+        //..
+
+        //返回结果
+        $result = array();
+        $result['status'] = 'opened';
+        return $this->generateCorrectResponse($result);
+    }
+
+    /**
+    *校验签名
+    **/
+    public function validate($params, $method)
+    {
+        $signature = $params['signature'];
+        unset($params['signature']);
+        ksort($params);
+        $uri = explode('?', $_SERVER['REQUEST_URI'])[0];
+        $raw_param = "$method\n$uri\n".http_build_query($params);
+        $sig = hash_hmac('sha256', $raw_param, CLIENT_SECRET, true);
+        $sigb64 = base64_encode($sig);
+        return $signature === urlencode($sigb64);
+    }
+
+    /**
+    *返回错误结果
+    **/
+    public function generateErrorResponse($msg)
+    {
+        return json_encode(['ret' => 1, 'msg' => $msg]);
+    }
+
+    /**
+    *返回正确结果
+    **/
+    public function generateCorrectResponse($result)
+    {
+        $result['ret'] = 0;
+        return json_encode($result);
+    }
+}
---
+++ isv.php
@@ -0,0 +1,45 @@
+<?php
+include('./Api.php');
+$api = new Api();
+
+$method = strtoupper($_SERVER['REQUEST_METHOD']);
+if($method == 'POST') {
+    $params = json_decode(file_get_contents('php://input'), true);
+}elseif ($method == 'GET') {
+    $params = $_GET;
+}
+
+//1. 校验参数（必传参数，参数格式...）
+if(!isset($params['type']) || !isset($params['signature'])) {
+    echo $api->generateErrorResponse('缺少参数');
+    exit();
+}
+
+//2. 校验签名
+if(!$api->validate($params, $method)) {
+    echo $api->generateErrorResponse('签名无效');
+    exit();
+}
+
+//3. 处理具体业务逻辑，返回结果
+switch ($params['type']) {
+    case 'OPEN':
+        echo $api->open();
+        break;
+    case 'CLOSE':
+        echo $api->close();
+        break;
+    case 'DELETE':
+        echo $api->delete();
+        break;
+    case 'RENEW':
+        echo $api->renew();
+        break;
+    case 'CHANGE':
+        echo $api->change();
+        break;
+
+    default:
+        echo $api->generateErrorResponse('未知操作');
+        break;
+}
